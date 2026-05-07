6f5992c0c3e4848356c2d3069026a9d7b50f8584
workaj <109080500+workaj@users.noreply.github.com>
2026-05-07 19:25:40 +0000

    Revert "chore: attempt at editor styling"
    This reverts commit 3525677b8b75f147ddbe706acb3d8850be3aac5e.

--- src/components/PuckEditor.tsx
+++ src/components/PuckEditor.tsx
@@ -1,6 +1,5 @@
 import { Puck } from "@puckeditor/core";
 import "@puckeditor/core/puck.css";
-import "../styles/puck-theme.css";
 import type { Config, Data } from "@puckeditor/core";
 
 const config: Config = {
