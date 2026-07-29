28ed2d9126a5bcb4596763c9aba1d7fc397cf4e7
workaj <109080500+workaj@users.noreply.github.com>
2026-07-29 18:43:34 +0000

    feat: implement saving for configuration settings and add turnstile

--- src/pages/admin/settings/form.ts
+++ src/pages/admin/settings/form.ts
@@ -13,7 +13,10 @@ const allTemplatesOptions = await db.select().from(templates);
 const inputClassList = "w-full rounded-md border border-gray-300 px-3 py-2 focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200 bg-base-100";
 const fields = getAllFields();
 
-export function getSettingsForm(): FormSection {
+export function getSettingsForm(defaultTemplateId?: string, turnstileSiteKey?: string, turnstileSecretKey?: string, flash: Record<string, string> = {}): FormSection {
+    const dtOptionValue = flash['dt-option'] ?? defaultTemplateId;
+    const turnstileSiteKeyValue = flash['turnstile-site-key'] ?? turnstileSiteKey;
+    const turnstileSecretKeyValue = flash['turnstile-secret-key'] ?? turnstileSecretKey;
     return {
         id: 'settings-form',
         title: 'Settings',
@@ -56,8 +59,71 @@ export function getSettingsForm(): FormSection {
                                 placeholder: 'Select a template',
                                 classList: inputClassList,
                                 optionsClassList: "bg-base-100 text-base-content",
+                                ...(dtOptionValue ? { value: dtOptionValue } : {}),
                                 required: true,
                                 validator: z.string().min(1, "Last name is required"),
+                            },
+                        ],
+                    }
+                ]
+            },
+            {
+                id: 'turnstile-group-wrapper',
+                name: 'turnstile-group-wrapper',
+                type: "Group",
+                fields: fields,
+                classList: "p-6 bg-base-100 rounded-lg",
+                groupFields: [
+                    {
+                        id: 'turnstile-group-header',
+                        name: 'turnstile-group-header',
+                        type: 'Html',
+                        markup: '<h2 class="text-lg font-medium">Turnstile</h2>',
+                    },
+                    {
+                        id: 'turnstile-site-key-group',
+                        name: 'turnstile-site-key-group',
+                        type: "Group",
+                        fields: fields,
+                        classList: "grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-6 mt-6",
+                        groupFields: [
+                            {
+                                id: 'turnstile-site-key-header',
+                                name: 'turnstile-site-key-header',
+                                type: 'Html',
+                                markup: '<h2 class="text-md font-medium flex items-center">Site Key</h2>',
+                            },
+                            {
+                                id: 'turnstile-site-key',
+                                name: 'turnstile-site-key',
+                                type: 'Input',
+                                classList: inputClassList,
+                                ...(turnstileSiteKeyValue ? { value: turnstileSiteKeyValue } : {}),
+                                validator: z.string().optional(),
+                            },
+                        ],
+                    },
+                    {
+                        id: 'turnstile-secret-key-group',
+                        name: 'turnstile-secret-key-group',
+                        type: "Group",
+                        fields: fields,
+                        classList: "grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-6 mt-6",
+                        groupFields: [
+                            {
+                                id: 'turnstile-secret-key-header',
+                                name: 'turnstile-secret-key-header',
+                                type: 'Html',
+                                markup: '<h2 class="text-md font-medium flex items-center">Secret Key</h2>',
+                            },
+                            {
+                                id: 'turnstile-secret-key',
+                                name: 'turnstile-secret-key',
+                                type: 'Input',
+                                inputType: 'password',
+                                classList: inputClassList,
+                                ...(turnstileSecretKeyValue ? { value: turnstileSecretKeyValue } : {}),
+                                validator: z.string().optional(),
                             },
                         ],
                     }
--- src/pages/admin/settings/update.ts
+++ src/pages/admin/settings/update.ts
@@ -2,54 +2,46 @@ import type { APIContext } from "astro";
 import { createAlert, alertType, addAlertToSession } from "../../../alert/index.js";
 import { validateForm, createUserAlertMessageFromArray, getFieldByName, formDataToRecord } from "../../../form/index.js";
 import { createFormFlashSession } from "../../../form/session.js";
-import { getAllFields } from "../../../form/index.js";
+import { getSettingsForm } from "./form.js";
 import { getDb } from "../../../db/db.js";
-import { users } from "../../../db/schema.js";
-import { eq } from "drizzle-orm";
-import { get } from "node:http";
+import { settings } from "../../../db/schema.js";
 
 export async function POST(context: APIContext): Promise<Response> {
     const db = getDb();
-    /* const userId = await context.session?.get('userId');
-    const [user] = await db.select().from(users).where(eq(users.id, userId)).limit(1);
-
-    if (!user) {
-        throw new Error('User not found');
-    }
-
-    const fields = getAllFields();
-    const form = getProfileForm(user, fields, '/admin/profile/update');
+    const form = getSettingsForm();
     const formData = await context.request.formData();
     const formFlash = createFormFlashSession(context.session);
     const result = validateForm(form, formData);
-    console.log(result);
 
     if (!result.success) {
-        await formFlash.set('profile', formDataToRecord(formData));
+        await formFlash.set('settings', formDataToRecord(formData));
         const errorMessage = createUserAlertMessageFromArray(form, result.errors);
         const alert = createAlert(alertType.error, errorMessage);
         await addAlertToSession(context.session, alert);
-        return context.redirect("/admin/profile");
+        return context.redirect("/admin/settings");
     }
 
-    const fname = getFieldByName(form, 'fname')?.value ?? user.fname;
-    const lname = getFieldByName(form, 'lname')?.value ?? user.lname;
-    const email = getFieldByName(form, 'email')?.value ?? user.email;
+    const defaultTemplateId = getFieldByName(form, 'dt-option')?.value;
+    const turnstileSiteKey = getFieldByName(form, 'turnstile-site-key')?.value ?? '';
+    const turnstileSecretKey = getFieldByName(form, 'turnstile-secret-key')?.value ?? '';
 
-    //check that email isnt used already by another user
-    const [existingUser] = await db.select().from(users).where(eq(users.email, email)).limit(1);
-    if (existingUser && existingUser.id !== user.id) {
-        await formFlash.set('profile', formDataToRecord(formData));
-        const alert = createAlert(alertType.error, "Email is already in use by another account.");
-        await addAlertToSession(context.session, alert);
-        return context.redirect("/admin/profile");
-    }
+    await db
+        .insert(settings)
+        .values({ key: 'default_template', value: defaultTemplateId })
+        .onConflictDoUpdate({ target: settings.key, set: { value: defaultTemplateId } });
 
-    //update fname/lname/email in db if it has changed
-    await db.update(users).set({ fname, lname, email }).where(eq(users.id, user.id));
+    await db
+        .insert(settings)
+        .values({ key: 'turnstile_site_key', value: turnstileSiteKey })
+        .onConflictDoUpdate({ target: settings.key, set: { value: turnstileSiteKey } });
 
-    await formFlash.delete('profile'); */
-    const alert = createAlert(alertType.info, "An update happened (no)!");
+    await db
+        .insert(settings)
+        .values({ key: 'turnstile_secret_key', value: turnstileSecretKey })
+        .onConflictDoUpdate({ target: settings.key, set: { value: turnstileSecretKey } });
+
+    await formFlash.delete('settings');
+    const alert = createAlert(alertType.success, "Settings updated successfully.");
     await addAlertToSession(context.session, alert);
 
     return context.redirect("/admin/settings");
